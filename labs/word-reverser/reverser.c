5ac4ad35f31ea827b83c7ac13eb852ea325f042c
Victor Guido <a01630603@itesm.mx>
2020-03-03 13:04:27 -0600

    Solucion word-reverser
    Signed-off-by: Victor Guido <a01630603@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
+    char c;
+    char palabra[50];
+    int contador = 0;
+    while((c = getchar()) != EOF){
+        if(c=='\n') {
+            contador--;
+            while(contador>=0) {
+                printf("%c", palabra[contador--]);
+            }
+            contador = 0;
+            printf("\n");
+        }
+        else {
+            palabra[contador++]=c;
+        }
+    }    
+
     return 0;
 }
