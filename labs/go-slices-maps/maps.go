3c1c30a70e6b0c18ed55b9688477cf43d3e6f2f6
Victor Guido <a01630603@itesm.mx>
2020-03-10 19:23:57 -0600

    Solved slices and maps
    Signed-off-by: Victor Guido <a01630603@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	counter := make(map[string]int)
+	for w := range words {		
+		counter[words[w]] = counter[words[w]] + 1
+	}
+	return counter
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+
+func Pic(dx, dy int) [][]uint8 {
+	var a[][]uint8
+	a = make([][]uint8, dy)
+	for i:= 0; i < dy; i++ {
+		a[i] = make([]uint8, dx)
+		for j:=0; j < dx; j++ {
+			a[i][j] = uint8((i^j * j^i))
+		}
+	}
+	return a
+}
+
 func main() {
+	pic.Show(Pic)
 }
