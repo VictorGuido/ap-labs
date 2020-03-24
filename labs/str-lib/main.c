a9340973889c4ddbedf6b59cd955cc78aa1c0933
Victor Guido <a01630603@itesm.mx>
2020-03-24 10:35:03 -0600

    Solved my string library
    Signed-off-by: Victor Guido <a01630603@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,20 @@
+///Victor Jaret Guido Alvarado
+//String library
+
 #include <stdio.h>
 
-int main() {
+char newString[150];
+
+
+int main(int argc, char *argv[])
+{
+    printf("Initial Lenght      : %i",mystrlen(argv[1]));
+    mystradd(argv[1],argv[2]);
+    if(mystrfind(newString, argv[3])==-1){
+        printf("\nSubstring was found: no\n");
+    }
+    else{
+        printf("\nSubstring was found : yes\n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,51 @@
+#include <string.h> 
 
+extern char newString[150];
 
 int mystrlen(char *str){
-    return 0;
+    int i =0;
+    int count=0;
+    while(str[i]!='\0'){ //aqui contamos que si haya caracter a leer
+        i++;
+        count++;
+    }
+    return count;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition){ 
+    int i = 0;
+    int j = 0;
+    printf("\nFirst string        : %s", origin); 
+    printf("\nSecond string       : %s", addition); 
+    while (origin[i] != '\0') { 
+        newString[j] = origin[i]; 
+        i++; 
+        j++; 
+    } 
+    i = 0; 
+    while (addition[i] != '\0') { 
+        newString[j] = addition[i]; 
+        i++; 
+        j++; 
+    } 
+    newString[j] = '\0'; 
+    printf("\nNew String          : %s", newString); 
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int M = mystrlen(substr); 
+    int N = mystrlen(origin); 
+    for (int i = 0; i <= N - M; i++) { 
+        int j; 
+        for (j = 0; j < M; j++){
+            if (origin[i + j] != substr[j]){
+                break; 
+            }
+        }
+        if (j == M){ 
+            return i;
+        }
+    } 
+    return -1; 
 }
+
