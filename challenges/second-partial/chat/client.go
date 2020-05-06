9162af1b8951ce94d9d45684a551ff94146b7220
Victor Guido <a01630603@itesm.mx>
2020-05-05 19:45:44 -0500

    Solved chat and makerfile
    Signed-off-by: Victor Guido <a01630603@itesm.mx>

--- challenges/second-partial/chat/client.go
+++ challenges/second-partial/chat/client.go
@@ -1,3 +1,5 @@
+//Victor Jaret Guido Alvarado
+// client.go
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
--- challenges/second-partial/chat/server.go
+++ challenges/second-partial/chat/server.go
@@ -1,5 +1,5 @@
 //Victor Jaret Guido Alvarado
-//main.go
+// main.go
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
